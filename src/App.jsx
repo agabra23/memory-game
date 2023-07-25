748eedabfd77f2cf89100194d9506ffc13cb4de0
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-07-24 20:05:14 -0400

    Render JSON data from endpoint into DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <h1>Hello World</h1>;
+  const ENDPOINT =
+    "https://api.giphy.com/v2/emoji?api_key=jLvjxLBegqMPTsPVnfAGVjxVZVqnkQdU&limit=10&offset=0";
+
+  const [display, setDisplay] = useState("");
+
+  useEffect(() => {
+    const fetchEmojis = async () => {
+      const response = await fetch(ENDPOINT);
+      const data = await response.json();
+      setDisplay(JSON.stringify(data, null, 6));
+      console.log(data);
+    };
+
+    fetchEmojis();
+  }, []);
+
+  return (
+    <p>
+      <pre>{display}</pre>
+    </p>
+  );
 }
 
 export default App;
