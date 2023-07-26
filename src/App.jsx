b18e08a41e6739ba3e36c503d3f01cac75282e59
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-07-26 08:59:17 -0400

    Set up page with layout and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
 import { useEffect, useState } from "react";
+import "./styles/App.css";
 
 function App() {
   const ENDPOINT =
     "https://api.giphy.com/v2/emoji?api_key=jLvjxLBegqMPTsPVnfAGVjxVZVqnkQdU&limit=10&offset=0";
 
   const [display, setDisplay] = useState("");
+  const [imageArray, setImageArray] = useState([]);
 
   useEffect(() => {
     const fetchEmojis = async () => {
-      const response = await fetch(ENDPOINT);
+      const response = await fetch(ENDPOINT, { mode: "cors" });
       const data = await response.json();
       setDisplay(JSON.stringify(data, null, 6));
-      console.log(data);
+
+      setImageArray(data.data);
     };
 
     fetchEmojis();
   }, []);
 
   return (
-    <p>
-      <pre>{display}</pre>
-    </p>
+    <>
+      <header>
+        <h1>Memory Game</h1>
+        <div className="scoreboard-container">
+          <h6>Score:</h6>
+          <h6>High Score:</h6>
+        </div>
+      </header>
+      <main>
+        {imageArray.map((item, index) => (
+          <div key={index}>
+            <button>
+              <img src={item.images?.original.url} alt={item.title} />
+            </button>
+          </div>
+        ))}
+      </main>
+    </>
   );
 }
 
