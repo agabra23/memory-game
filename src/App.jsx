60f2debb9c0d19e515d66f73e6c9419fb0b3a74c
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-07-26 10:44:51 -0400

    Reorganized the side effect that fetches data

--- src/App.jsx
+++ src/App.jsx
@@ -7,40 +7,33 @@ function App() {
     "https://api.giphy.com/v2/emoji?api_key=jLvjxLBegqMPTsPVnfAGVjxVZVqnkQdU&limit=30&offset=0";
 
   const [selectedImages, setSelectedImages] = useState([]);
+  const [allImages, setAllImages] = useState([]);
+
+  const fetchEmojis = async () => {
+    try {
+      const response = await fetch(ENDPOINT, { mode: "cors" });
+      const data = await response.json();
+      setAllImages(data.data);
+    } catch (error) {
+      console.error("Error fetching emojis:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchEmojis = async () => {
-      try {
-        const response = await fetch(ENDPOINT, { mode: "cors" });
-        const data = await response.json();
-
-        // Move the selection of four random images inside this useEffect
-        const gridImages = [];
-        for (let i = 0; i < 4; i++) {
-          gridImages.push(
-            data.data[Math.floor(Math.random() * data.data.length)]
-          );
-        }
-        setSelectedImages(gridImages);
-      } catch (error) {
-        console.error("Error fetching emojis:", error);
-      }
-    };
-
     fetchEmojis();
   }, []);
 
-  // useEffect(() => {
-  //   const fourImages = [];
-  //   for (let i = 0; i < 4; i++) {
-  //     fourImages.push(imageArray[Math.floor(Math.random() * 31)]);
-  //   }
-  //   setSelectedImages([...fourImages]);
-  // }, [imageArray]);
-
   useEffect(() => {
-    console.log(selectedImages);
-  }, [selectedImages]);
+    if (allImages.length > 0) {
+      const gridImages = [];
+      for (let i = 0; i < 4; i++) {
+        gridImages.push(
+          allImages[Math.floor(Math.random() * allImages.length)]
+        );
+      }
+      setSelectedImages(gridImages);
+    }
+  }, [allImages]);
 
   return (
     <>
